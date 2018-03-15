fe89e408f41e7b4bf8836c9a56dd9950e2293376
gongzza <gz.us.to@gmail.com>
2018-03-15 17:21:45 +0900

    Refactor index.js
    set
    - `Object.hasOwnProperty`를 이용해서 command에서 필요하던 파라메터를 줄였습니다.
    
    update
    - `$set` 명령이 바로 올 경우 체크
    - 수정된 set 메소드에 맞춰서 파라메터 제거
    - 변수명 수정

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
 
 // recursive function
-function set(context, commands, prop, target) {
-  Object.keys(commands).forEach(cmd => {
-    if (cmd === '$set') {
-      context[prop] = Object.assign({}, context[prop], {[target]: commands[cmd]})
+function set(context, commands, prop) {
+  Object.keys(commands).forEach(target => {
+    const command = commands[target]
+    if(command.hasOwnProperty('$set')) {
+      context[prop] = Object.assign({}, context[prop], {[target]: command['$set']})
     } else {
-      // recursive call
-      set(context, commands[cmd], target, cmd)
+      // TODO context[prop] is undefined.
+
+      set(context[prop], command, target)
     }
   })
 }
 
 function update(state, commands) {
 
+  if (commands.hasOwnProperty('$set')) {
+    return commands['$set']
+  }
+  
   // soft copy
   const nextState = Object.assign({}, state)
 
-  Object.keys(commands).forEach(cmd => {
-    set(nextState, commands[cmd], cmd, cmd)
+  Object.keys(commands).forEach(target => {
+    set(nextState, commands[target], target)
   })
 
   return nextState
