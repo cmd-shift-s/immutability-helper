a5d30ba7a9ad88692e85fcbc82fcf9d87aa60fee
gongzza <gz.us.to@gmail.com>
2018-03-15 16:29:05 +0900

    Pass test
    재귀 함수를 이용해서 뎁스가 깊어져도 테스트를 통과할 수 있도록 만들었습니다.

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 
+// recursive function
+function set(context, commands, prop, target) {
+  Object.keys(commands).forEach(cmd => {
+    if (cmd === '$set') {
+      context[prop] = Object.assign({}, context[prop], {[target]: commands[cmd]})
+    } else {
+      // recursive call
+      set(context, commands[cmd], target, cmd)
+    }
+  })
+}
+
 function update(state, commands) {
+
+  // soft copy
+  const nextState = Object.assign({}, state)
+
+  Object.keys(commands).forEach(cmd => {
+    set(nextState, commands[cmd], cmd, cmd)
+  })
+
+  return nextState
 }
 
 module.exports = update
